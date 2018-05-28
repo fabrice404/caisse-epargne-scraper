6c75236da21c331932d526ff77028942378b2b97
Fabrice Lamant <flamant@shutterstock.com>
2018-05-28 07:16:04 +0100

    CE scraper

---
+++ README.md
@@ -0,0 +1,32 @@
+# caisse-epargne-scraper
+
+Download balance and latest transactions from Caisse d'Epargne's website.
+
+## Getting Started
+
+
+### Prerequisites
+
+To run this module, you will need to download the latest chrome driver [here](http://chromedriver.storage.googleapis.com/index.html).
+
+
+### Usage
+
+```javascript
+const caisseEpargne = require('caisse-epargne-scraper');
+
+const result = await caisseEpargne.get({
+  login:    'YOUR LOGIN',
+  password: 'YOUR PASSWORD',
+});
+```
+
+
+## Built With
+
+* [Selenium](https://www.npmjs.com/package/selenium-webdriver): browser automation library
+* [Cheerio](https://www.npmjs.com/package/cheerio): jQuery implementation in Node.js
+
+## License
+
+This project is licensed under the [Apache 2.0 License](https://www.apache.org/licenses/LICENSE-2.0)
---
+++ index.js
@@ -0,0 +1,142 @@
+const cheerio = require('cheerio');
+const { Builder, By, until } = require('selenium-webdriver');
+
+const defaultConfig = {
+  url: 'https://www.caisse-epargne.fr/bretagne-pays-de-loire/particuliers',
+  browser: 'chrome',
+};
+
+module.exports = {
+  get: async (_config) => {
+    const config = Object.assign(defaultConfig, _config);
+    const driver = new Builder()
+      .forBrowser(config.browser)
+      .build();
+
+    const getElement = async (selector) => {
+      const by = selector.startsWith('/') ? By.xpath(selector) : By.id(selector);
+      await driver.wait(until.elementLocated(by));
+      return driver.wait(until.elementIsVisible(driver.findElement(by)));
+    };
+
+    const getContent = async (selector) => {
+      const element = await getElement(selector);
+      return element.getAttribute('innerHTML');
+    };
+
+    const getAmountFromText = text => parseFloat(text.replace(/,/g, '.').replace(/[^0-9.-]/g, ''));
+
+    const click = async (selector) => {
+      const element = await getElement(selector);
+      await element.click();
+    };
+
+    const loaderVisibility = async () => {
+      return new Promise((resolve, reject) => {
+        let xpath = `//div[contains(@class, "loaderServeur")]`
+        driver.wait(until.elementLocated(By.xpath(xpath))).then(() => {
+          driver.findElement(By.xpath(xpath)).getCssValue(`display`)
+            .then(display => {
+              if (display === `block`) {
+                setTimeout(() => { loaderVisibility().then(resolve) }, 100)
+              } else {
+                resolve()
+              }
+            })
+            .catch(console.log)
+        })
+      })
+    };
+
+    const result = [];
+    try {
+      // navigate to url
+      await driver.get(config.url);
+
+      // click button 'Accès aux comptes'
+      click('//a[contains(@class, "icon-bpce-profil")]');
+
+      // wait for login button visibility
+      await getElement('//div[contains(@class, "modal-body")]//form[contains(@class, "identification-form")]/button[@type="submit"]');
+
+      // sleep for half a second because the login input is cleared by javascript after modal opening
+      await driver.sleep(500);
+
+      // set login input value
+      await driver.executeScript((login) => {
+        document.getElementById('idClient').value = login;
+      }, config.login);
+
+      // click login button
+      click('//div[contains(@class, "modal-body")]//form[contains(@class, "identification-form")]/button[@type="submit"]');
+
+      // wait for keypad visibility
+      await getElement('//div[contains(@class, "type-password") and contains(@class, "in")]/form/div[contains(@class, "affClavierSecurise")]');
+
+      // set password input value
+      await driver.executeScript((password) => {
+        document.getElementById('input_password_accessibility').parentElement.style.display = 'block'
+        document.getElementById('codconfstar').parentElement.style.display = 'none'
+        document.getElementById('input_password_accessibility').value = password
+      }, config.password);
+
+      // click password button 
+      click('//div[contains(@class, "type-password") and contains(@class, "in")]/form/div[contains(@class, "affClavierClassique")]/div/button[@type="submit"]');
+
+      // list accounts
+      const accountIds = [];
+      let content = await getContent('//div[@id="MM_ContentMain"]');
+      let $ = cheerio.load(content);
+
+      $('.rowClick a[target="_self"]').each((i, a) => {
+        accountIds.push($(a).parent().text().trim());
+      });
+
+      for (let i = 0; i < accountIds.length; i += 1) {
+        const id = accountIds[i];
+
+        // click account button
+        await getElement(`//td[text()="${id}"]`);
+        click(`//td[text()="${id}"]`)
+
+        // wait while loader is visible
+        await loaderVisibility();
+
+        const balanceElement = await getContent('//span[contains(@class, "bigFont")]')
+        const mainElement = await getContent('//div[@id = "MM_ContentMain"]')
+
+        $ = cheerio.load(mainElement);
+        const balance = getAmountFromText($('#MM_HISTORIQUE_COMPTE .somme .bigFont').text());
+        const done = [];
+        $('table.msi-table tr.rowClick').each((i, row) => {
+          done.push({
+            date: $(row).children().eq(0).text(),
+            name: $(row).children().eq(1).text(),
+            amount: getAmountFromText($(row).children().eq(2).text() + $(row).children().eq(3).text())
+          })
+        })
+
+        // click button MA SYTHESE
+        await getElement('//a[contains(@href, "CPTSYNT0")]')
+        click('//a[contains(@href, "CPTSYNT0")]')
+
+        // wait while loader is visible
+        await loaderVisibility();
+
+        result.push({
+          name: id,
+          balance,
+          available: balance,
+          transactions: { done, pending: [] },
+        });
+      }
+
+      if (!config.keepItOpen) {
+        driver.quit();
+      }
+    } catch (ex) {
+      throw ex;
+    }
+    return result;
+  },
+};
